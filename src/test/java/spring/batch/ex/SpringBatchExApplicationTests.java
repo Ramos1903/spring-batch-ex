d3cf14e4708b921e663fbd3535c12f945b6b9e12
Vinicius <vinicius.jr08@hotmail.com>
2023-05-13 17:33:41 -0300

    initial commit

---
+++ src/test/java/spring/batch/ex/SpringBatchExApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.batch.ex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
